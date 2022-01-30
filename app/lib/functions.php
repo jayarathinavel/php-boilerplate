ac524522916997bea53881128c37423d92ade6ec
jayarathinavel <jayrathinavel@gmail.com>
2022-01-30 12:09:13 +0530

    Changes in Page Name

--- app/lib/functions.php
+++ app/lib/functions.php
@@ -1,5 +1,16 @@
 <?php
-function getPageName(){
-  return ($_SERVER['REQUEST_URI'] != '/') ? $_SERVER['REQUEST_URI'] : 'home';
-}
+
+  function getPageName(){
+    $pageName = $_SERVER['REQUEST_URI'];
+    if(containsWord($pageName, '?')){
+      $pageNameSplit = explode('?', $pageName);
+      $pageName = $pageNameSplit[0];
+    }
+    return ($pageName != '/') ? $pageName : 'home';
+  }
+
+  function containsWord($str, $word){
+    return !!preg_match('#\\b' . preg_quote($word, '#') . '\\b#i', $str);
+  }
+
 ?>
